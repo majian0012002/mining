49ffe2a6010f810232f8cdb7e8b653d85bff4389
lanjing <lanjing.mj@antfin.com>
2018-03-08 23:41:14 +0800

    wadawdwad

--- src/main/java/com/miller/mining/MiningApplication.java
+++ src/main/java/com/miller/mining/MiningApplication.java
@@ -3,6 +3,8 @@ package com.miller.mining;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -10,9 +12,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan("com.miller.mining.mapper")
 @EnableCaching
-public class MiningApplication {
+public class MiningApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MiningApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(MiningApplication.class);
 	}
 }
